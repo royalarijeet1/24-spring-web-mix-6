86cf260708de1c242195a24717793a0604cd3483
PayF123 <141803727+royalarijeet1@users.noreply.github.com>
2024-06-14 23:16:32 +0530

    insert user

---
+++ src/main/java/com/dao/EUserDao.java
@@ -0,0 +1,21 @@
+package com.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.bean.EUserBean;
+
+@Repository
+public class EUserDao {
+	
+	@Autowired // instance eject
+	JdbcTemplate stmt; // update() query()
+
+	public void insertUser(EUserBean userBean) {
+		// query ->
+		stmt.update("insert into users(firstName,email,password) values (?,?,?) ", userBean.getFirstName(),
+				userBean.getEmail(), userBean.getPassword());
+
+	}
+}
